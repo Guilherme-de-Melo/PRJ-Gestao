047eba3b6eb79b6d38b33e86b7b66ff007e1c324
osmar <osmarhenriquecn@hotmail.com>
2024-11-18 21:33:21 -0300

    Declaração das models

---
+++ src/main/java/org/example/PlacaDeVideo.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class PlacaDeVideo {
+
+    private int id;
+    private String fabricante;
+    private String modelo;
+    private double tdp;
+
+    public PlacaDeVideo(int id, String fabricante, String modelo, double tdp){        
+        this.id = id;
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+        this.tdp = tdp;
+    }
+    
+}
---
+++ src/main/java/org/example/PlacaMae.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class PlacaMae {
+
+    private int id;
+    private String fabricante;
+    private String soquete;
+    private boolean podeOverclock;
+
+    public PlacaMae(int id, String fabricante, String soquete, boolean podeOverclock) {
+        this.id = id;
+        this.fabricante = fabricante;
+        this.soquete = soquete;
+        this.podeOverclock = podeOverclock;
+    }
+
+}
